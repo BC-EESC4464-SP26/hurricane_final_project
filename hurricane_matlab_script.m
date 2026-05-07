5b8c235b0730465bd42c55bb4d0d7b5b2fdff053
sararigatti <rigattis@bc.edu>
2026-05-07 11:28:45 -0400

    adding comments

--- hurricane_matlab_script.m
+++ hurricane_matlab_script.m
@@ -12,7 +12,7 @@ for i = 1:length(filenames)
 end
 water_lvl=table2array(allData(:,5));
 
-%% Correct the Dates
+%% Correct the Dates (water level data)
 
 date_day = allData(:,1);
 date_day_array = table2array(date_day);
@@ -31,10 +31,8 @@ hours_extended = repmat(hours, (height(allData)/24), 1);
 dates = (datenum(num_day) + hours_extended);
 dates_full = NaN(length(dates), 1);
 dates_full(:,1) = dates; %final time variable for water levels
-%%baseline_index = find(dates_full == 736696);
-%%baseline_three_year = mean(water_lvl(1:baseline_index));
 
-%% Correcting Hurricane Dates
+%% Correct the Dates (Hurricane data)
 
 Hurricane_data_for_dates=readmatrix('Storm Track Data.xlsx'); %date, lat
 Hurricane_data_for_names=readtable('Storm Track Data.xlsx');
@@ -42,7 +40,9 @@ Hurricane_data_for_names=readtable('Storm Track Data.xlsx');
 dates_hurricane= Hurricane_data_for_dates(:,1)+datenum('01-00-1900');
 names_hurricane = string(table2array(Hurricane_data_for_names(:,8)));
 
-%% 
+%% Question 1: Are hurricanes recorded in water level?
+
+% Preliminary visualization of water level (not a final figure)
 figure (1); clf
 title('water level')
 plot(dates_full, water_lvl, ".")
@@ -50,14 +50,16 @@ datetick('x','mmm yyyy')
 
 pred_water_lvl = table2array(allData(:,3));
 anomaly = water_lvl - pred_water_lvl;
+% calculate water level anomaly using the predicted water level from the
+% dataset
 smoothed_anom = movmean(anomaly, 6);
 
 figure (2); clf
 title('Water Level Anomaly')
 for i = 1:length(dates_hurricane)
-    %xline(dates_hurricane(i), lineWidth=2, Color = [0.8, 0.8, 0.8])
     xline(dates_hurricane(i), '-', names_hurricane(i), lineWidth=2, Color = [0.8, 0.8, 0.8])
 end
+% plot each hurricane date as a semi-transparent vertical line
 hold on
 plot(dates_full, smoothed_anom, ".")
 ylabel("Water Level Anomaly (ft)")
@@ -66,23 +68,28 @@ datetick('x','mmm yyyy')
 legend({'Hurricane (Category)', '', '', '', '', '', '', '', 'Water Level'}, Location = "southwest")
 hold off
 
-%% Question 2
+%% Question 2: What is the water level anomaly associated with each storm?
 
 hurricane_max = NaN(8,1);
+% create an empty array to store the maximum water level from each storm
 
 for i = 1:length(dates_hurricane)
     min_date = dates_hurricane(i) - 0.5;
+    % finds the date 12 hours before the closest hurricane measurement
     max_date = dates_hurricane(i) + 0.5;
+    % finds the date 12 hours after the closest hurricane measurement
     min_index = find(abs(dates_full-min_date) < 0.001);
+    max_index = find(abs(dates_full-max_date) < 0.001);
     %use a tolerance of 0.001 to find the index of min_date and max_date
     %since the actual values are not exactly equal
-    max_index = find(abs(dates_full-max_date) < 0.001);
     hurricane_range = water_lvl(min_index:max_index,:);
     hurricane_max(i,1) = max(hurricane_range);
 end
 
 figure (3); clf
 b = bar(flip(names_hurricane), flip(hurricane_max));
+% arrays must be flipped since they are listed in reverse chronological
+% order
 title("Max Water Level From Each Hurricane")
 xtips = b.XEndPoints;
 ytips = b.YEndPoints;
@@ -91,21 +98,28 @@ text(xtips, ytips, labels, 'HorizontalAlignment', 'center','VerticalAlignment', 
 xlabel("Hurricanes (oldest to most recent)")
 ylabel("Water Level (ft)")
 
-%% Question 3
+%% Question 3: Does strength, intensity, or both combined have more of an impact on water level?
 
 strength_cats = Hurricane_data_for_dates(:,7);
+% extract strength category from original dataset
 lats_hurricane = Hurricane_data_for_dates(:,2);
 lons_hurricane = -1.*Hurricane_data_for_dates(:,3);
 
 gi_lat = 29.2633;
 gi_lon = -89.9566;
+% hard-coded lat and lon for grand isle tide station
 
 dists = NaN(8,1);
 dist_cats = NaN(8,1);
+% generate empty arrays for distance and distance category rankings
 
 for i = 1:length(lats_hurricane)
     arclen = distance(gi_lat, gi_lon, lats_hurricane(i), lons_hurricane(i));
+    % distance finds the arc distance between the nearest hurricane
+    % measurement and the grand isle tide station based on latitude and
+    % longitude coordinates
     dist = deg2km(arclen);
+    % converts the arc distance in degrees to km
     dists(i) = dist;
     if dist <= 23
         dist_cats(i) = 5;
@@ -121,12 +135,16 @@ for i = 1:length(lats_hurricane)
 end
 
 intensity = dist_cats .* strength_cats;
+% calculate aggregate intensity score by multiplying
 
 instant_water_level = NaN(8,1);
 for i = 1:8
     instant_index = find(abs(dates_full-dates_hurricane(i)) < 0.001);
     instant_water_level(i) = water_lvl(instant_index);
 end
+% instant water level represents the water level at the instant that the
+% hurricane was measured. However, the max water level was what we ended up
+% plotting.
 
 figure(4); clf
 subplot(3,1,1)
@@ -139,16 +157,19 @@ text(2.1, 2.2, "Francine (H1)",'FontSize',8, 'Color', [0, 0.4470, 0.7410])
 text(1.1, 1, "Claudette (TS)",'FontSize',8, 'Color', [0, 0.4470, 0.7410])
 text(4.1, 1.5, "Zeta (H3)",'FontSize',8, 'Color', [0, 0.4470, 0.7410])
 text(5, 3.25, "Ida (H4)",'FontSize',8, 'Color', [0, 0.4470, 0.7410])
+% manual label locations for ease of viewing plots
 hold on
 p1 = polyfit(strength_cats, hurricane_max, 1); 
 x1 = linspace(0,6,100);
 y1 = polyval(p1, x1);
 plot(x1, y1, "--",'LineWidth', 2)
+% calculate the slope and plot it as a line
 slope1 = p1(1);
 text(0.15, 2, sprintf('Slope = %.2f', slope1))
 [r1, pval1] = corr(strength_cats, hurricane_max);
 r2_1 = r1^2;
 text(0.15, 1, sprintf('R^2 = %.2f\np = %.3f', r2_1, pval1))
+% calculate the r^2 and p value to test the validity of the relationship
 xlim([0,6])
 xlabel("Strength Category")
 ylim([0,4])
@@ -164,11 +185,13 @@ p2 = polyfit(dist_cats, hurricane_max, 1);
 x2 = linspace(0,6,100);
 y2 = polyval(p2, x2);
 plot(x2, y2, "--",'LineWidth', 2)
+% calculate the slope and plot it as a line
 slope2 = p2(1);
 text(0.15, 1.8, sprintf('Slope = %.2f', slope2))
 [r1, pval1] = corr(dist_cats, hurricane_max);
 r2_1 = r1^2;
 text(0.15, 1, sprintf('R^2 = %.2f\np = %.3f', r2_1, pval1))
+% calculate the r^2 and p value to test the validity of the relationship
 xlim([0,6])
 xlabel("Distance Category")
 ylim([0,4])
@@ -186,17 +209,19 @@ text(1, 2.1, "Francine (H1)",'FontSize',8, 'Color', [0, 0.4470, 0.7410])
 text(2.1, 1, "Claudette (TS)",'FontSize',8, 'Color', [0, 0.4470, 0.7410])
 text(12.1, 1.5, "Zeta (H3)",'FontSize',8, 'Color', [0, 0.4470, 0.7410])
 text(19.5, 3.6, "Ida (H4)",'FontSize',8, 'Color', [0, 0.4470, 0.7410])
-%text(intensity, hurricane_max, names_hurricane,'VerticalAlignment','bottom','HorizontalAlignment','center','FontSize',8, 'Color', [0, 0.4470, 0.7410])
+% manual label locations for ease of viewing plots
 hold on
 p3 = polyfit(intensity, hurricane_max, 1); 
 x3 = linspace(0,21,100);
 y3 = polyval(p3, x3);
 plot(x3, y3, "--",'LineWidth', 2)
+% calculate the slope and plot it as a line
 slope3 = p3(1);
 text(16, 2, sprintf('Slope = %.2f', slope3))
 [r1, pval1] = corr(dist_cats, hurricane_max);
 r2_1 = r1^2;
 text(16, 1, sprintf('R^2 = %.2f\np = %.3f', r2_1, pval1))
+% calculate the r^2 and p value to test the validity of the relationship
 xlim([0,21])
 xlabel("Intensity Category (Strength * Distance)")
 ylim([0,4])
